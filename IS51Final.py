be5738251c6179ffb55a1bf78f15d27a0745be2a
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-05-19 16:00:06 -0700

    finish structured_english

---
+++ IS51Final.py
@@ -0,0 +1,5 @@
+"""
+The program is trying to take students grades from a text file called "Final.txt".
+It will display the number of grades, the average grade, and the percentage of grades that are above the average grade
+We will use three different functions to obtain the three different grades. 
+"""
